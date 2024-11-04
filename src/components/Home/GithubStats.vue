b6a9415f4955510da9328f4dc78493b451d78c51
Junaid-PK <junaidhussnain369@gmail.com>
2024-11-04 05:36:52 +0500

    github stats responsiveness

--- src/components/Home/GithubStats.vue
+++ src/components/Home/GithubStats.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="flex flex-col items-center justify-center">
-    <div class="flex justify-center">
+    <div class="flex flex-col md:flex-row justify-center">
       <img
+        class="w-full md:w-auto"
         src="https://github-readme-stats.vercel.app/api?username=Junaid-PK&theme=shadow_green&hide_border=true&include_all_commits=false&count_private=false"
         alt="GitHub Stats"
         width="386"
       />
       <img
+        class="w-full md:w-auto mt-4 md:mt-0"
         src="https://github-readme-stats.vercel.app/api/top-langs/?username=Junaid-PK&theme=shadow_green&hide_border=true&include_all_commits=false&count_private=false&layout=compact"
         alt="Top Languages"
       />
     </div>
     <img
+      class="w-full md:w-auto mt-4"
       src="https://github-readme-streak-stats.herokuapp.com/?user=Junaid-PK&theme=shadow_green&hide_border=true"
       alt="GitHub Streak Stats"
     />
